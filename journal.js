0466b577fba50814959692bff57fcbdc62e897ee
Izarra Villareal <izarravill@gmail.com>
2025-07-06 21:27:56 -0700

    Add isEven() function to journal

--- journal.js
+++ journal.js
@@ -2,3 +2,9 @@
 function greet(name) {
   return "Hello, " + name + "!";
 }
+
+// Check if a number is even
+// This function takes a number as input and returns true if it is even
+function isEven(number) {
+  return number % 2 === 0;
+}
